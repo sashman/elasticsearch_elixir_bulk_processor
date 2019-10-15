8d9d3707444ab9b5a5a45d457ca745eb4a6bc33b
sashman <sashman90@gmail.com>
2019-10-15 11:48:24 +0100

    Switching to function ref

--- lib/elasticsearch_elixir_bulk_processor/bulk/elastic.ex
+++ lib/elasticsearch_elixir_bulk_processor/bulk/elastic.ex
@@ -1,14 +1,13 @@
 defmodule ElasticsearchElixirBulkProcessor.Bulk.Elastic do
-
   def bulk_upload(data, cluster) when is_list(data) do
+    bulk_data =
+      data
+      |> Enum.map_join("\n", &Poison.encode!/1))
 
-    bulk_data = data
-    |> Enum.map_join("\n", &(Poison.encode(&1)))
-
-    {:ok, res} = ElasticSearch.post(cluster, "/_bulk", bulk_data);
+    {:ok, res} = ElasticSearch.post(cluster, "/_bulk", bulk_data)
   end
 
   def bulk_upload(data, cluster) when is_map(data) do
-    {:ok, res} = ElasticSearch.post(cluster, "/_bulk", data);
+    {:ok, res} = ElasticSearch.post(cluster, "/_bulk", data)
   end
 end
