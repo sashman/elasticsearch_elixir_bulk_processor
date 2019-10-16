5f4ae5d11d1f10ecb100d1348f8154e2b9cb2d48
sashman <sashman90@gmail.com>
2019-10-16 15:17:58 +0100

    Adding spec to bulk upload

--- lib/elasticsearch_elixir_bulk_processor/bulk/elastic.ex
+++ lib/elasticsearch_elixir_bulk_processor/bulk/elastic.ex
@@ -1,4 +1,5 @@
 defmodule ElasticsearchElixirBulkProcessor.Bulk.Elastic do
+  @spec bulk_upload(list, Elasticsearch.Cluster.t(), fun, fun) :: Task.t()
   def bulk_upload(data, cluster, success_fun \\ & &1, error_fun \\ & &1)
       when is_list(data) and
              is_function(success_fun) and
