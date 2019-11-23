951d43493c78f8bc1998bc194fcd417c25283f06
sashman <sashman90@gmail.com>
2019-11-23 23:47:15 +0000

    New insert test task

--- lib/mix/tasks/insert_test.ex
+++ lib/mix/tasks/insert_test.ex
@@ -1,43 +1,33 @@
 defmodule Mix.Tasks.InsertTest do
   use Mix.Task
-  alias ElasticsearchElixirBulkProcessor.Bulk.Client
+  alias ElasticsearchElixirBulkProcessor.{Bulk, Items}
 
   @shortdoc "Test insertion using Bulk module"
-  def run([count]) do
+  def run([count, per_bulk]) do
     {:ok, _started} = Application.ensure_all_started(:elasticsearch_elixir_bulk_processor)
 
     {count, _} =
       count
       |> Integer.parse()
 
+    {per_bulk, _} =
+      per_bulk
+      |> Integer.parse()
+
     count
-    |> insert()
+    |> insert(per_bulk)
   end
 
-  def run(_) do
-    {:ok, _started} = Application.ensure_all_started(:elasticsearch_elixir_bulk_processor)
-    insert(1)
-  end
-
-  defp insert(count) do
-    data =
-      for i <- 1..100,
-          do: [
-            %{"index" => %{"_index" => "test"}},
-            %{"test#{rem(i, 999)}" => "test#{i}"}
-          ]
-
+  defp insert(count, per_bulk) do
     1..count
-    |> Enum.map(fn _ ->
-      Task.async(fn ->
-        Client.bulk_upload(
-          data |> List.flatten(),
-          ElasticsearchElixirBulkProcessor.ElasticsearchCluster,
-          &IO.inspect/1,
-          &IO.inspect("***ERROR***\n#{inspect(&1)}***ERROR***\n\n")
-        )
-      end)
+    |> Enum.each(fn _ ->
+      index_item("test", %{"a" => "test string"})
+      |> List.duplicate(per_bulk)
+      |> Bulk.DirectUpload.add_requests()
     end)
-    |> Enum.map(fn task -> Task.await(task) end)
+  end
+
+  defp index_item(index_name, payload) do
+    %Items.Index{index: index_name, source: payload}
   end
 end
