373f0353a99fcca23a88de4d099f95148e847f7d
sashman <sashman90@gmail.com>
2019-11-26 19:27:51 +0000

    Insert task now times the insert

--- lib/mix/tasks/insert_test.ex
+++ lib/mix/tasks/insert_test.ex
@@ -14,14 +14,23 @@ defmodule Mix.Tasks.InsertTest do
       per_bulk
       |> Integer.parse()
 
-    count
-    |> insert(per_bulk)
+    base_line_doc_total = count_current_docs()
+
+    Task.async(fn ->
+      count
+      |> insert(per_bulk)
+    end)
+
+    :timer.tc(fn ->
+      wait_until_doc_count(count * per_bulk, base_line_doc_total)
+    end)
+    |> IO.inspect()
   end
 
   defp insert(count, per_bulk) do
     1..count
     |> Enum.each(fn _ ->
-      index_item("test", %{"a" => "test string"})
+      index_item("test", index_payload())
       |> List.duplicate(per_bulk)
       |> Bulk.DirectUpload.add_requests()
     end)
@@ -29,5 +38,61 @@ defmodule Mix.Tasks.InsertTest do
 
   defp index_item(index_name, payload) do
     %Items.Index{index: index_name, source: payload}
+  end
+
+  defp index_payload() do
+    {int, _} = Integer.parse(Randomizer.randomizer(10, :numeric))
+
+    %{
+      "my_string0" => Randomizer.randomizer(20),
+      "my_string1" => int,
+      "my_string2" => Randomizer.randomizer(50),
+      "my string3" => random_enum(),
+      "my_string4" => Randomizer.randomizer(20),
+      "my_string5" => Randomizer.randomizer(20),
+      "my_string6" => Randomizer.randomizer(20),
+      "my_string7" => Randomizer.randomizer(20),
+      "my_string8" => Randomizer.randomizer(20),
+      "my_string9" => Randomizer.randomizer(20)
+    }
+  end
+
+  defp random_enum do
+    [
+      "STRING",
+      "ANOTHER STRING",
+      "SOMETHING ELSE"
+    ]
+    |> Enum.random()
+  end
+
+  defp wait_until_doc_count(doc_count, base_line, state \\ %{})
+  defp wait_until_doc_count(_, _, %{retry: 360}), do: {:error, :timeout}
+
+  defp wait_until_doc_count(doc_count, base_line, state) do
+    cond do
+      doc_count + base_line == count_current_docs() ->
+        {:ok}
+
+      true ->
+        state[:retry]
+        |> case do
+          nil ->
+            wait_until_doc_count(doc_count, base_line, %{retry: 0})
+
+          retry ->
+            :timer.sleep(1000)
+            wait_until_doc_count(doc_count, base_line, %{retry: retry + 1})
+        end
+    end
+  end
+
+  defp count_current_docs do
+    ElasticsearchElixirBulkProcessor.ElasticsearchCluster
+    |> Elasticsearch.post("test/_search?track_total_hits=true", %{size: 0})
+    |> case do
+      {:ok, %{"hits" => %{"total" => %{"value" => total}}}} -> total
+      _ -> 0
+    end
   end
 end
