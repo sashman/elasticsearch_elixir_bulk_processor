d670e94b139413ffaec40af66b673047d46d8b4a
sashman <sashman90@gmail.com>
2019-12-07 12:39:03 +0000

    Updating test script to focus on high insert low bulk numbers

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for INSERT_COUNT in {100..1000..100} 
+for INSERT_COUNT in {1000..10000..1000} 
 do
 
-    for BULK_COUNT in {500..5000..500} 
+    for BULK_COUNT in {50..500..50} 
     do
         echo "insert: $INSERT_COUNT bulk: $BULK_COUNT"
         mix insert_test $INSERT_COUNT $BULK_COUNT staged >> log_staged
