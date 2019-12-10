d323ace72467acc15feac768cf4560e4b152938e
sashman <sashman90@gmail.com>
2019-12-10 22:23:15 +0000

    Adding eliver

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ElasticsearchElixirBulkProcessor.MixProject do
   def project do
     [
       app: :elasticsearch_elixir_bulk_processor,
-      version: "0.1.0",
+      version: String.trim(File.read!("VERSION")),
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -33,6 +33,7 @@ defmodule ElasticsearchElixirBulkProcessor.MixProject do
       {:poison, "~> 3.1"},
       {:gen_stage, "~> 0.14"},
       {:excoveralls, "~> 0.10", only: :test},
+      {:eliver, "~> 2.0", only: :dev},
       {:size, "~> 0.1.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
