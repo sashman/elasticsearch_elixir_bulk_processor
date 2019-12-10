c4a6a6a9b19f8fb312d385490960aed9ed5cd694
sashman <sashman90@gmail.com>
2019-12-10 23:30:33 +0000

    Top level func to set threshold

--- lib/elasticsearch_elixir_bulk_processor.ex
+++ lib/elasticsearch_elixir_bulk_processor.ex
@@ -1,18 +1,20 @@
 defmodule ElasticsearchElixirBulkProcessor do
+  alias ElasticsearchElixirBulkProcessor.Bulk.BulkStage
+
   @moduledoc """
   Documentation for ElasticsearchElixirBulkProcessor.
   """
 
   @doc """
-  Hello world.
+  Set the maximum number of bytes to send to elasticsearch per bulk request.
 
   ## Examples
 
-      iex> ElasticsearchElixirBulkProcessor.hello()
+      iex> ElasticsearchElixirBulkProcessor.set_byte_threshold(10)
       :world
 
   """
-  def hello do
-    :world
+  def set_byte_threshold(bytes) do
+    BulkStage.set_byte_threshold(bytes)
   end
 end
