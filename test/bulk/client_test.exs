4184756164ab8cfce17e5070c4f450da65978536
sashman <sashman90@gmail.com>
2019-12-13 23:06:16 +0000

    Adding test case for multiple errors

--- test/bulk/client_test.exs
+++ test/bulk/client_test.exs
@@ -1,10 +1,15 @@
 defmodule ElasticsearchElixirBulkProcessor.Bulk.ClientTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
   import Mock
 
   alias ElasticsearchElixirBulkProcessor.Bulk.Client
 
   defmodule FunctionStub do
+    def error_fun(_) do
+    end
+  end
+
+  defmodule FunctionStub2 do
     def error_fun(_) do
     end
   end
@@ -22,6 +27,25 @@ defmodule ElasticsearchElixirBulkProcessor.Bulk.ClientTest do
           )
 
           assert_called(FunctionStub.error_fun(:_))
+        end
+      end
+    end
+
+    test "calls error function when there is multiple errors" do
+      with_mock Elasticsearch,
+        post: fn _, _, _ -> {:ok, %{"errors" => true, "items" => ["one", "two", "three"]}} end do
+        with_mock FunctionStub2, error_fun: fn _ -> :ok end do
+          Client.bulk_upload(
+            "",
+            ElasticsearchElixirBulkProcessor.ElasticsearchCluster,
+            & &1,
+            &FunctionStub2.error_fun/1
+          )
+
+          :timer.sleep(100)
+          assert_called(FunctionStub2.error_fun("one"))
+          assert_called(FunctionStub2.error_fun("two"))
+          assert_called(FunctionStub2.error_fun("three"))
         end
       end
     end
