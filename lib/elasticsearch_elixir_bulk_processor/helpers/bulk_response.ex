9e9257efbbfcf144db16248238ac19d708a72861
sashman <sashman90@gmail.com>
2019-12-19 20:58:14 +0000

    Adding docs test to gather items given a string payload

--- lib/elasticsearch_elixir_bulk_processor/helpers/bulk_response.ex
+++ lib/elasticsearch_elixir_bulk_processor/helpers/bulk_response.ex
@@ -1,4 +1,17 @@
 defmodule ElasticsearchElixirBulkProcessor.Helpers.BulkResponse do
+  @doc ~S"""
+
+  Given a list of items from a bulk response and the data sent as a string payload return the items that match the error.
+
+  ## Examples
+
+    iex>
+    ...> items = [%{"index" => %{}}}, %{"index" => %{"error" => _}}}, %{"index" => %{}}}]
+    ...> data = "item\nitem_with_errors\nitem"
+    ...> ElasticsearchElixirBulkProcessor.Helpers.BulkResponse.gather_error_items(items, data)
+    ["item_with_errors"]
+
+  """
   def gather_error_items(items, data) when is_binary(data) do
     gather_error_items(
       items,
@@ -9,7 +22,7 @@ defmodule ElasticsearchElixirBulkProcessor.Helpers.BulkResponse do
 
   @doc ~S"""
 
-  Split list of strings into first chunk of given byte size and rest of the list.
+  Given a list of items from a bulk response and the data sent as a list of requests return the items that match the error.
 
   ## Examples
 
