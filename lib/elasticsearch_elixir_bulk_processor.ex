bc0fefccbdf222262f6a815c3bd0fa771d28c57f
sashman <sashman90@gmail.com>
2019-12-28 23:36:18 +0000

    Docs

--- lib/elasticsearch_elixir_bulk_processor.ex
+++ lib/elasticsearch_elixir_bulk_processor.ex
@@ -1,9 +1,75 @@
 defmodule ElasticsearchElixirBulkProcessor do
-  alias ElasticsearchElixirBulkProcessor.Bulk.BulkStage
+  alias ElasticsearchElixirBulkProcessor.Bulk.{BulkStage, Upload}
 
   @moduledoc """
-  Documentation for ElasticsearchElixirBulkProcessor.
+  Elasticsearch Elixir Bulk Processor is a configurable manager for efficiently inserting data into Elasticsearch.
+  This processor uses genstages for handling backpressure, and various settings to control the bulk payloads being uploaded to Elasticsearch.
+
+  Inspired by the [Java Bulk Processor](https://www.elastic.co/guide/en/elasticsearch/client/java-api/current/java-docs-bulk-processor.html)
+
+  ## Configuration
+
+  ### Action count
+
+  Number of actions/items to send per bulk (can be changed at run time)
+
+  ```
+  ElasticsearchElixirBulkProcessor.set_event_count_threshold(100)
+  ```
+
+  ### Byte size
+
+  Max number of bytes to send per bulk (can be changed at run time)
+
+  ```
+  ElasticsearchElixirBulkProcessor.set_byte_threshold(100)
+  ```
+
+  ### Action order
+
+  Preservation of order of actions/items
+
+  ```
+  config :elasticsearch_elixir_bulk_processor, preserve_event_order: false
+  ```
+
+  ### Retries
+
+  Retry policy, this uses the [ElixirRetry](https://github.com/safwank/ElixirRetry) DSL. See `ElasticsearchElixirBulkProcessor.Bulk.Retry.policy`.
+
+  ```
+  config :elasticsearch_elixir_bulk_processor, retry_function: &MyApp.Retry.policy/0
+  ```
+
+
+  ### Success and error handlers
+
   """
+
+  @doc """
+  Send a list of request items to ELasticsearch. This mechanism uses GenStages for back pressure.
+  NOTE: It should be completely reasonable to use this function by passing single element lists, the mechanism aggregates the items together prior to sending them.
+
+  The list elements much be structs:
+    * `ElasticsearchElixirBulkProcessor.Items.Index`
+    * `ElasticsearchElixirBulkProcessor.Items.Create`
+    * `ElasticsearchElixirBulkProcessor.Items.Update`
+    * `ElasticsearchElixirBulkProcessor.Items.Delete`
+
+  ## Examples
+
+      iex> alias ElasticsearchElixirBulkProcessor.Items.Index
+      ...> [
+      ...>  %Index{index: "test", source: %{"field" => "value1"}}
+      ...>  %Index{index: "test", source: %{"field" => "value2"}}
+      ...>  %Index{index: "test", source: %{"field" => "value3"}}
+      ...> ]
+      ...> |> ElasticsearchElixirBulkProcessor.send_requests()
+
+  """
+  def send_requests(bulk_items) when is_list(bulk_items) do
+    Upload.add_requests(bulk_items)
+  end
 
   @doc """
   Set the maximum number of bytes to send to elasticsearch per bulk request.
@@ -18,6 +84,15 @@ defmodule ElasticsearchElixirBulkProcessor do
     BulkStage.set_byte_threshold(bytes)
   end
 
+  @doc """
+  Set the maximum count of items to send to elasticsearch per bulk request.
+
+  ## Examples
+
+      iex> ElasticsearchElixirBulkProcessor.set_byte_threshold(10)
+      :ok
+
+  """
   def set_event_count_threshold(count) do
     BulkStage.set_event_count_threshold(count)
   end
