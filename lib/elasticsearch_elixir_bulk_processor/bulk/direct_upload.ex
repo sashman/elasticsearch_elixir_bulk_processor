9c9aa55196902eb5451b9813ea9472ba767d9346
sashman <sashman90@gmail.com>
2021-05-06 13:59:46 +0100

    Adding config handlers to direct upload

--- lib/elasticsearch_elixir_bulk_processor/bulk/direct_upload.ex
+++ lib/elasticsearch_elixir_bulk_processor/bulk/direct_upload.ex
@@ -1,10 +1,24 @@
 defmodule ElasticsearchElixirBulkProcessor.Bulk.DirectUpload do
-  alias ElasticsearchElixirBulkProcessor.Bulk.Client
+  alias ElasticsearchElixirBulkProcessor.Bulk.{Client, Handlers}
 
   def add_requests(bulk_requests) do
+    success_fun =
+      Application.get_env(
+        :elasticsearch_elixir_bulk_processor,
+        :success_function,
+        &Handlers.default_success/1
+      )
+
+    error_fun =
+      Application.get_env(
+        :elasticsearch_elixir_bulk_processor,
+        :error_function,
+        &Handlers.default_error/1
+      )
+
     bulk_requests
     |> Stream.map(& &1.__struct__.to_payload(&1))
     |> Enum.join("\n")
-    |> Client.bulk_upload()
+    |> Client.bulk_upload(success_fun, error_fun)
   end
 end
